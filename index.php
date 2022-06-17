86867ebe524a3c88d886c20e219fc85b86b2e1a5
sushmitha-sys <68849782+sushmitha-sys@users.noreply.github.com>
2022-06-17 15:37:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -51,5 +51,49 @@ $num=mysqli_num_rows($connect);
         }
         ?>
     </table>
+    <form  >
+    <p>Insert the new Movie Details</p>
+  <label for="mov_name">Movie name:</label><br>
+  <input type="text" id="mov_name" name="mov_name"><br>
+  <label for="actror">Actor name:</label><br>
+  <input type="text" id="actror" name="actror"><br>
+  <label for="actress">Actress name:</label><br>
+  <input type="text" id="actress" name="actress"><br>
+  <label for="Year">Released Year:</label><br>
+  <input type="text" id="Year" name="Year"><br>
+  <label for="Director">Director name:</label><br>
+  <input type="text" id="Director" name="Director"><br><br>
+  <input type="submit" name="submit" value="Submit Information" class="btn btn-primary">
+</form>
+<?php
+
+if(isset($_POST['submit']))
+{
+    //get all the details from the form
+    $mov_name=$_POST['mov_name'];
+    $actror=$_POST['actror'];
+    $actress=$_POST['actress'];
+    
+    $year=$_POST['Year'];
+    
+    $director=$_POST['Director'];
+    
+
+    $sql2="INSERT INTO movies SET 
+            mov_name='$mov_name',
+            actror=$actror,
+            actress=$actress,
+            year=$Year,
+            
+            director='$Director',
+            
+            ";
+        //echo $sql2;die();
+    $res2=mysqli_query($conn,$sql2);
+}
+?>
+
+
 </div>
 </body>
+</html>
